583a4a101b096487a675234fa7658c6ac9afafe0
Ellen Bauer <ellen.bauer@elmastudio.de>
2023-10-20 20:53:24 +1300

    Add 404 image

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -7,8 +7,7 @@
 ?>
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|9","left":"var:preset|spacing|9"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--9);padding-left:var(--wp--preset--spacing--9)">
-<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|9"}}}} -->
+<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--9);padding-left:var(--wp--preset--spacing--9)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|9"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|9"}}} -->
 <div class="wp-block-column"><!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"lineHeight":"1","letterSpacing":"-12px"},"spacing":{"padding":{"bottom":"var:preset|spacing|6"}}},"textColor":"font-primary","className":"has-text-align-center pb__6 has-font-primary-color has-text-color has-text-xxxl-font-size","fontSize":"text-9-xl"} -->
 <h1 class="wp-block-heading has-text-align-left has-text-align-center pb__6 has-font-primary-color has-text-color has-text-xxxl-font-size has-text-9-xl-font-size" style="padding-bottom:var(--wp--preset--spacing--6);letter-spacing:-12px;line-height:1">404</h1>
@@ -26,10 +25,9 @@
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":2556,"aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"24px"}}} -->
-<figure class="wp-block-image size-large has-custom-border"><img src="http://themes.elmastudio.de/moog/files/2023/09/Real-Size-GUNDAM-683x1024.jpg" alt="" class="wp-image-2556" style="border-radius:24px;aspect-ratio:1;object-fit:cover"/></figure>
+<div class="wp-block-column"><!-- wp:image {"id":31206,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"24px"}}} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/thunder-robot.jpg" alt="" class="wp-image-31206" style="border-radius:24px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns -->
-</div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
